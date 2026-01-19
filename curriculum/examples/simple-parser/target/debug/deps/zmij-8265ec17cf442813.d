8a2550e25a38ca8bbfd6853e6f2fa3040a08c720
s3cur1typh0x <iambyansi@gmail.com>
2026-01-19 09:01:53 +0300

    Revert "Scaffold comprehensive Detection-as-Code curriculum with Rust"

--- curriculum/examples/simple-parser/target/debug/deps/zmij-8265ec17cf442813.d
+++
@@ -1,9 +0,0 @@
-/home/runner/work/Detection-as-Code/Detection-as-Code/curriculum/examples/simple-parser/target/debug/deps/zmij-8265ec17cf442813.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/stdarch_x86.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/traits.rs
-
-/home/runner/work/Detection-as-Code/Detection-as-Code/curriculum/examples/simple-parser/target/debug/deps/libzmij-8265ec17cf442813.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/stdarch_x86.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/traits.rs
-
-/home/runner/work/Detection-as-Code/Detection-as-Code/curriculum/examples/simple-parser/target/debug/deps/libzmij-8265ec17cf442813.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/stdarch_x86.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/traits.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/stdarch_x86.rs:
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.14/src/traits.rs:
